9f1ada5257f3adb7b8b24dfe2aa97ce9af6273c9
Spirit38 <mathis.vangi@gmail.com>
2025-04-10 15:08:01 +0200

    Fix responsive

---
+++ script.js
@@ -0,0 +1,16 @@
+const burger = document.getElementById('burger');
+const navLinks = document.querySelector('.nav-links');
+const burgerIcon = document.getElementById('burger-icon');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+
+  // Change icône entre burger et X
+  if (navLinks.classList.contains('active')) {
+    burgerIcon.classList.remove('fa-bars');
+    burgerIcon.classList.add('fa-times');
+  } else {
+    burgerIcon.classList.remove('fa-times');
+    burgerIcon.classList.add('fa-bars');
+  }
+});
